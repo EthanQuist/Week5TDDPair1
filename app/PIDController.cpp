7246e0cd3355c9a7ae3b76fd3a040b5979078124
rjb3vp <rjb3vp@virginia.edu>
2019-09-25 13:26:54 -0400

    Fixed math for compute()

--- app/PIDController.cpp
+++ app/PIDController.cpp
@@ -7,15 +7,25 @@
 
 double PIDController::compute(double actualVelocity, double setpointVelocity){
 
+/*
+error = setpoint - measured_value
+integral = integral + error * dt
+derivative = (error - previous_error) / dt
+output = Kp * error + Ki * integral + Kd * derivative
+
+We have assumed integral and previous error to be 0.0, dt to be 0.5 as mentioned in the activity diagram.
+*/
 	double dt = 0.5;
 	double previousError = 0.0;
 	double initialIntegral = 0.0;
 
-	double error = actualVelocity - setpointVelocity;
+	double error = setpointVelocity - actualVelocity;
+
+	
 
 	double proportionalAspect = Kp * error;
-	double integralAspect = Ki * initialIntegral;
-	double derivativeAspect = Kd * (error - previousError);
+	double integralAspect = Ki * (initialIntegral + error * dt);
+	double derivativeAspect = Kd * (error - previousError) / dt;
 
 
 	return proportionalAspect + integralAspect + derivativeAspect;
