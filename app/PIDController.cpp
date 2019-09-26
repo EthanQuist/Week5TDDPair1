ce9e29e54e8e6fda46ae9ab96e0a53eb7deca81e
rjb3vp <rjb3vp@virginia.edu>
2019-09-26 14:22:40 -0400

    Added a comment

--- app/PIDController.cpp
+++ app/PIDController.cpp
@@ -7,7 +7,7 @@
 
 double PIDController::compute(double actualVelocity, double setpointVelocity) {
   double dt = 0.5;
-  double previousError = 0.0;
+  double previousError = 0.0; //starts as zero, but here for completeness' sake
   double initialIntegral = 0.0;
   double error = setpointVelocity - actualVelocity;
   double proportionalAspect = Kp * error;
